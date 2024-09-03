724f152a4a1237150f79cec67a3d3d6d5ed40bfa
王小白 <731896502@qq.com>
2024-09-03 14:44:10 +0800

    完善模板

--- src/components/Header.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <header class="w-full py-8 hidden">
-    <!-- <div class="mx-auto">
-      test
-    </div> -->
-  </header>
-</template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,6 @@
 <script lang="ts" setup>
-import Header from '@/components/Header.vue'
-import Main from '@/components/Main.vue'
-// import Footer from '@/components/Footer.vue'
+
 </script>
 <template>
-  <Header />
-  <Main />
-  <!-- <Footer /> -->
+  <div>some</div>
 </template>
